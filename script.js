4120f18723206638965cddfabf65f1faf2516bc8
ayanda <64249187+ayandagatsha@users.noreply.github.com>
2021-08-03 19:25:55 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btn = document.getElementById('log-btn');
 
 btn.addEventListener('click', ()=>{
 
-    window.location.href = 'https://ayandagatsha.github.io/SRS-Saver-Prototype/';
+    window.location.href = 'https://ayandagatsha.github.io/SRS-Saver-Prototype/dashboard';
   
   })
 
