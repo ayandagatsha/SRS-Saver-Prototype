5e1c0a1de2676e64e0f68d58a8c7878c30842160
ayanda <64249187+ayandagatsha@users.noreply.github.com>
2021-08-03 19:35:56 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const btn = document.getElementById('log-btn');
 
 btn.addEventListener('click', ()=>{
 
-    window.location.href = 'dashhboard.html';
+   location.href = 'dashhboard.html';
   
   })
 
